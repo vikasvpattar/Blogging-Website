0132ad8348366d435f7e162f60c22297737dbecf
Vikas V Pattar <81294766+vikasvpattar@users.noreply.github.com>
2023-12-16 10:09:48 +0530

    .env file of client and cors has been changed

--- api/index.js
+++ api/index.js
@@ -32,8 +32,8 @@ const uploadmiddleware = multer({ dest: "uploads/" });
 const cookieParser = require("cookie-parser");
 
 // This wil allow the front end to access the backend
-// app.use(cors({ credentials: true, origin: process.env.ORIGIN }));
-app.use(cors({ credentials: true }));
+app.use(cors({ credentials: true, origin: process.env.ORIGIN }));
+// app.use(cors({ credentials: true,origin:"*" }));
 
 app.use(express.json());
 app.use(cookieParser());
